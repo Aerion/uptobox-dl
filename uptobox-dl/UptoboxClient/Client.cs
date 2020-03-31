f1a432ab2d6e3b734b9073ec1d4f31bc98964fb2
Guillaume Taquet Gasperini <perso@aerion.me>
2020-03-31 11:31:39 +0200

    add uri builder in client and use uris

--- uptobox-dl/UptoboxClient/Client.cs
+++ uptobox-dl/UptoboxClient/Client.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
@@ -16,9 +18,10 @@ namespace UptoboxDl.UptoboxClient
         private static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions()
             {AllowTrailingCommas = true};
 
-        private string _fileCode { get; }
-        private HttpClient _client;
+        private readonly string _fileCode;
+        private readonly HttpClient _client;
         private readonly string _userToken;
+        private readonly Uri _baseAddress;
 
         /// <summary>
         /// Wrapper around Uptobox API
@@ -27,15 +30,16 @@ namespace UptoboxDl.UptoboxClient
         /// <param name="userToken">Token of the Uptobox account https://docs.uptobox.com/?javascript#how-to-find-my-api-token</param>
         /// <param name="hostname">Uptobox domain, should be uptobox.com</param>
         /// <param name="useHttps">Set to false to use http instead of https</param>
-        /// <param name="customHttpClient">Specify a custom httpclient for requests to be handled</param>
+        /// <param name="customHttpClient">Custom HttpClient to use</param>
         public Client(string fileCode, string userToken, string hostname = "uptobox.com", bool useHttps = true,
             HttpClient customHttpClient = null)
         {
             _userToken = userToken;
             _fileCode = fileCode;
             _client = customHttpClient ?? new HttpClient();
+
             var scheme = useHttps ? "https" : "http";
-            _client.BaseAddress = new Uri($"{scheme}://{hostname}/api/");
+            _baseAddress = new Uri($"{scheme}://{hostname}");
         }
 
         /// https://docs.uptobox.com/?javascript#get-a-waiting-token
@@ -48,51 +52,41 @@ namespace UptoboxDl.UptoboxClient
         public async Task<WaitingToken> GetWaitingTokenAsync(string password,
             CancellationToken cancellationToken = default)
         {
-            var url = $"link?token={_userToken}&file_code={_fileCode}";
-            if (password != null)
-            {
-                url += $"&password={password}";
-            }
-
-            return await GetAsync<WaitingToken>(url, cancellationToken).ConfigureAwait(false);
+            var uri = GetUri("link", $"token={_userToken}", $"file_code={_fileCode}",
+                password == null ? string.Empty : $"password={password}");
+            return await GetAsync<WaitingToken>(uri, cancellationToken).ConfigureAwait(false);
         }
 
         /// https://docs.uptobox.com/?javascript#get-the-download-link
         public async Task<Uri> GetDownloadLinkAsync(WaitingToken waitingToken,
             CancellationToken cancellationToken = default)
         {
-            var url = $"link?token={_userToken}&file_code={_fileCode}&waitingToken={waitingToken.Token}";
-            var data = await GetAsync(url, cancellationToken).ConfigureAwait(false);
+            var uri = GetUri("link", $"token={_userToken}", $"file_code={_fileCode}",
+                $"waitingToken={waitingToken.Token}");
+            var data = await GetAsync(uri, cancellationToken).ConfigureAwait(false);
 
-            return new Uri(data.GetProperty("dlLink").GetString(), UriKind.Absolute);
+            return new Uri(data.GetProperty("dlLink").GetString());
         }
 
-        private async Task<JsonElement> GetAsync(string url, CancellationToken ct = default, bool retryOnTimeout = true)
+        private async Task<JsonElement> GetAsync(Uri uri, CancellationToken ct = default)
         {
-            try
-            {
-                var response = await _client.GetAsync(url, ct).ConfigureAwait(false);
-                var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+            var response = await _client.GetAsync(uri, ct).ConfigureAwait(false);
+            var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
 
-                return DeserializeJsonElementFromStream(stream);
-            }
-            catch (TimeoutException)
-            {
-                // Retry once on timeout error
-                return await GetAsync(url, ct, false);
-            }
+            return DeserializeJsonElementFromStream(stream);
         }
 
-        private async Task<T> GetAsync<T>(string url, CancellationToken ct = default)
+        private async Task<T> GetAsync<T>(Uri uri, CancellationToken ct = default)
         {
-            var elt = await GetAsync(url, ct).ConfigureAwait(false);
+            var elt = await GetAsync(uri, ct).ConfigureAwait(false);
             return JsonSerializer.Deserialize<T>(elt.GetRawText(), JsonSerializerOptions);
         }
 
         private static JsonElement DeserializeJsonElementFromStream(Stream stream)
         {
             using var sr = new StreamReader(stream);
-            var data = JsonSerializer.Deserialize<ResponseBase>(sr.ReadToEnd(), JsonSerializerOptions);
+            var strData = sr.ReadToEnd();
+            var data = JsonSerializer.Deserialize<ResponseBase>(strData, JsonSerializerOptions);
 
             if (InvalidStatusCodes.Contains(data.StatusCode))
             {
@@ -100,6 +94,31 @@ namespace UptoboxDl.UptoboxClient
             }
 
             return data.Data;
+        }
+
+        /// <summary>
+        /// Get Uptobox URI by passing a path and multiple query elements
+        /// </summary>
+        /// <param name="path">Path without domain part</param>
+        /// <param name="queryElements">Elements with "key=value" to be passed as query string</param>
+        /// <returns>Built uri with domain prepended and query elements added</returns>
+        private Uri GetUri(string path, params string[] queryElements)
+        {
+            var builder = new UriBuilder(_baseAddress) {Path = $"api/{path}"};
+            if (queryElements.Length == 0)
+            {
+                return builder.Uri;
+            }
+
+            var queryBuilder = new StringBuilder();
+            queryBuilder.Append($"?{queryElements[0]}");
+            for (var i = 1; i < queryElements.Length; i++)
+            {
+                queryBuilder.Append($"&{queryElements[i]}");
+            }
+
+            builder.Query = queryBuilder.ToString();
+            return builder.Uri;
         }
     }
 
